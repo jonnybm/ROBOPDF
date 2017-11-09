47b0663e7566fa460b6857f02c7c1258eb247265
Jonny Bergara Musy <bergaramusy@gmail.com>
2017-11-09 02:02:05 -0200

    Comit Robo Pdf renomear PDF caixa e BB

---
+++ src/RoboPDF.java
@@ -0,0 +1,531 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.Normalizer;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
+
+public class RoboPDF {
+	
+	
+	private static String banco = "BB";  // BB ou CEF
+	
+	public static String caminho = "";  // BB ou CEF
+	public static String destino = "";  // BB ou CEF
+	public static String gravarValor = ""; 	
+	public static String gravarCJ = "";
+	
+	public static int apagarTerceiraTentativaCorrompido = 0;
+
+
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+
+			
+			visualizarArquivos();
+			
+			
+            }
+
+	public static void init() throws IOException, InterruptedException {
+		
+		visualizarArquivos();
+		
+	}
+	
+	
+	  public static void visualizarArquivos() throws IOException, InterruptedException {
+		  
+		  System.out.println("LENDO ORIGEM : "+caminho);
+		  System.out.println("LENDO DESTINO: "+destino);
+
+		  	String caminhoNovo = "";
+		  	String caminhoNew = "";
+		  	String caminhoNewBB = "";
+
+		  	//caminho = "/Volumes/HD/BackupJonny/Projetos/EficienciaFinanceira/PDFBB/";
+		  	//destino = "/Volumes/HD/BackupJonny/Projetos/EficienciaFinanceira/PDFBB/";
+		  	String arquivoPDF = "";
+		  
+		  
+		  	File file = new File(caminho);
+			File afile[] = file.listFiles();
+			int i = 0;
+			
+			for (int j = afile.length; i < j; i++) {
+				
+				File arquivos = afile[i];
+				arquivoPDF = caminho+arquivos.getName();
+				System.out.println(arquivos.getName());
+				
+				if(arquivos.getName().indexOf ("pdf") >= 0) {
+				
+					//Recebe e Le Texto do PDF
+					String texto = extraiTextoDoPDF(arquivoPDF);
+					
+					System.out.println("CONTEUDO: "+texto);
+					
+					
+					if(!texto.equals("NAOePDF") || texto != "NAOePDF") 
+					{
+						//Recebe o Conteudo do PDF e coloca em Array com quebra de Linha
+						String linhas[] = texto.split("\n");
+						
+				        					        			
+						//Trata o PDF lendo linha a linha do array
+						String textoret = trataPDF(linhas);
+						
+						 System.out.println("CONTEUDO: "+textoret);
+						
+						//visualizarArquivos();
+						
+						
+						//caminhoNovo = destino+textoret.toUpperCase();
+						caminhoNovo="";
+						
+						caminhoNovo = caminhoNovo.concat(destino);
+						caminhoNovo = caminhoNovo.concat(textoret.toUpperCase());
+						//caminhoNovo = caminhoNovo+".pdf";
+						
+//						//System.out.println(caminhoNovo);
+//						caminhoNovo.replace("\\", "");
+//						caminhoNovo.replace("//", "");
+//						//System.out.println(caminhoNovo);
+						
+						
+						//LER CARACTER POR CARACTER PARA PODER FUNCIONAR NO WINDOWS  (CAMOMINHO NOME E VALOR)
+						caminhoNew = "";
+						
+						Scanner s = new Scanner(caminhoNovo);
+						
+						String nome = s.nextLine();
+						
+						for(int i1 = 0; i1 < nome.length(); i1++) 
+						{
+						  //System.out.println(nome.charAt(i1));
+						  
+						  caminhoNew = caminhoNew+nome.charAt(i1);
+						  
+						  if((i1+1 == nome.length()) ) 
+						  {
+							  
+							if(banco.equals("BB"))
+						  	{
+								caminhoNew = caminhoNew+" "+gravarCJ+" "+gravarValor; 
+								
+						  	}
+							else if(banco.equals("CEF"))
+							{
+								caminhoNew = caminhoNew+" "+gravarValor+".pdf";
+							}	
+							  
+						  }
+						}
+						
+						//LER CARACTER POR CARACTER PARA PODER FUNCIONAR NO WINDOWS  (PATH + PDF DO BB)
+						caminhoNewBB = "";
+						
+						if(banco.equals("BB"))
+					  	{
+							s = new Scanner(caminhoNew);
+							
+							nome = s.nextLine();
+							
+							for(int i1 = 0; i1 < nome.length(); i1++) 
+							{
+							  //System.out.println(nome.charAt(i1));
+							  
+								caminhoNewBB = caminhoNewBB+nome.charAt(i1);
+							  
+							  if((i1+1 == nome.length()) ) 
+							  {
+								  
+								if(banco.equals("BB"))
+							  	{
+									caminhoNewBB = caminhoNewBB+".pdf"; 
+									
+									
+							  	}
+							  }
+							}
+							caminhoNew = caminhoNewBB;
+					  	}
+						
+						
+						System.out.println("VAI RENOMEAR PARA: "+caminhoNew+"\n" );
+						
+						arquivos.renameTo(new File(caminhoNew));
+												
+						Thread.sleep( 3000 );
+						
+//						  if(arquivoPDF.indexOf ("DS_Store") <= 0) //Se arquivo for diferente de arquivo de sistema que nao precosa ser analizado
+//						  {
+//							  arquivos.renameTo(new File(destino, textoret.toUpperCase()+".pdf"));
+//						  }						
+					}
+					
+
+				}
+				//Renomeia os arquivos.  
+				//arquivos.renameTo(new File("/Volumes/HD/BackupJonny/Projetos/EficienciaFinanceira/PDF", textoret+".pdf"));
+			} 	
+	  	}
+  
+
+    		
+		  public static String extraiTextoDoPDF(String caminho) throws InterruptedException {
+			  
+			  //ARQUIVOS OCULTOS MAC
+			  if(caminho.indexOf ("DS_Store") >= 0) {
+				  
+				  return "NAOePDF";
+			  }
+
+			  //ARQUIVOS OCULTOS WINDOWS
+			  if(caminho.indexOf ("desktop") >= 0 || caminho.indexOf ("desktop.ini") >= 0) {
+				  
+				  return "NAOePDF";
+			  }
+
+			  // SE NAO FOR ARQUIVO PDF
+			  if(caminho.indexOf ("pdf") < 0) {
+				  return "NAOePDF";
+			  }
+			  
+			  
+			    PDDocument pdfDocument = null;
+			    
+		        boolean ok = true;
+		        do {
+				    try {
+				      pdfDocument = PDDocument.load(caminho);
+				      PDFTextStripper stripper = new PDFTextStripper();
+				      String texto = stripper.getText(pdfDocument);
+				      return texto;
+				    } 
+				    catch (IOException e) 
+				    {
+				    		System.out.println("ERRO - LEITURA DO PDF:"+ caminho); 
+				    		
+				    		Thread.sleep( 5000 );				    		
+				    		
+				    		ok = false;
+				    		
+			    			File f = new File(caminho);
+			    			f.delete();
+			    			
+			    			System.out.println("APAGOU ARQUIVO CORROMPIDO : "+ caminho);
+				    		
+//				    		apagarTerceiraTentativaCorrompido ++;
+//				    		
+//				    		if(apagarTerceiraTentativaCorrompido == 3)
+//				    		{
+//				    			apagarTerceiraTentativaCorrompido = 0;
+//				    		}	
+				    		
+				    		//throw new RuntimeException(e);
+				    		break;
+				    		//System.exit(0);
+				    } 
+				    finally 
+				    {
+				    		if (pdfDocument != null) 
+				    			try 
+				    			{
+				    				pdfDocument.close();
+				    			} 
+				    			catch (IOException e) 
+				    			{
+				    				System.out.println("ERRO FECHAR LEITURA DO PDF");
+				    				ok = false;
+				    			}
+				    }
+		        
+				//    return "NAOePDF";
+		        	}
+		        	while (!ok);
+		        		return "NAOePDF";
+		  		}  
+				    //return "NAOePDF";
+		  
+	
+		  
+
+		  
+		  
+		  
+		  public static String trataPDF(String[] linhas) 
+		  {
+			  
+			  String ret = "";
+			  String retorno = "";
+			  
+			  
+				  //if(linhas[15].equals("------------------------------------------------") || linhas[15].trim() == "------------------------------------------------" || linhas[15] == "------------------------------------------------") //(banco.equals("BB")) // Banco do Brasil
+			  		if(banco.equals("BB"))
+			  		{
+					  System.out.println("EXTRATO BANCO DO BRASIL");
+					  
+						  
+					  for (int i = 0; i < linhas.length; i++) 
+					  {
+						
+						  if(i == 2) //LINHA 2 Primeira Parte - PEGA a CONTA JUDICIAL
+				          {
+				            		//ret = linhas[i];				            	
+				            		
+				            		//ret = ret.substring(26, ret.length() -12);  //FAZ SUBSTRING PARA TIRAR O NUMERO DE PARCELA
+							  
+							  	gravarCJ	= "";
+				            		
+				            		gravarCJ = linhas[i];
+				            		gravarCJ = gravarCJ.substring(26, gravarCJ.length() -12);  //FAZ SUBSTRING PARA TIRAR O NUMERO DE PARCELA
+				            		gravarCJ = gravarCJ.replace("C", "");
+				            		gravarCJ = gravarCJ.replace("c", "");
+				            		gravarCJ = gravarCJ.replace("P", "");
+				            		gravarCJ = gravarCJ.replace("p", "");
+				            		gravarCJ =  semAcento(gravarCJ.trim());
+				            		//System.out.println("RETT 1 = " + gravarCJ);
+				            		
+				          }
+						  
+						  
+						  if(i == 8) //Linha 8 Caixa pega o Nome 
+				          {
+				            		ret = linhas[i] ;
+				            		//ret = linhas[i] +" CJ"+ret
+				            		System.out.println("RETT 2 = " + ret);
+				          }
+						  
+						//System.out.println("CONTEDUDO "+linhas[i]);
+
+						  
+				          if(linhas.length-1 == i)//é o ultimo  Registro?
+				          {
+				        	  	gravarValor = "";	
+				        	  
+				        	  	if(!linhas[i].equals("Voltar")) //Existem extratoq eu aultima linha tem o descricao voltar 
+				        	  	{
+				        	  		//ret = ret +" "+linhas[i];
+				        	  		gravarValor = linhas[i];
+				        	  	}
+				        	  	else
+				        	  	{
+				        	  		//ret = ret +" "+linhas[i-1];
+				        	  		gravarValor = linhas[i-1];
+				        	  	}	
+				        	  	
+				        	  	gravarValor = gravarValor.replace("C", "");
+				        	  	gravarValor = gravarValor.replace("c", "");
+				        	  	gravarValor = gravarValor.replace("Autor          :", "");
+				        	  	gravarValor = gravarValor.replace("         Saldo do período             ", "");
+				        	  	gravarValor = gravarValor.replace("          Saldo do período            ", "");
+				        	  	gravarValor = gravarValor.replace("    Saldo do periodo    ", "");
+				        	  	gravarValor = gravarValor.replace("/", "");
+				        	  	gravarValor = gravarValor.replace("                ", "");
+				        	  	gravarValor = gravarValor.replace("Reclamante", "");
+				        	  	gravarValor = gravarValor.replace(":", "");
+				        	  	gravarValor = gravarValor.replace("     ", "");
+				        	  	gravarValor = gravarValor.replace("     Saldo do periodo ", "");
+				        	  	gravarValor = gravarValor.replace("Autor", "");
+				        	  	gravarValor = gravarValor.replace("     Saldo do periodo    ", "");
+				        	  	gravarValor = gravarValor.replace("     Saldo do periodo    ", "");
+				        	  	gravarValor = gravarValor.replace("Saldo do periodo", "");
+				        	  	gravarValor = gravarValor.replaceAll("[^\\p{ASCII}]", "");
+				        	  	gravarValor = gravarValor.replaceAll("//", "");
+				        	  	gravarValor = gravarValor.replaceAll("/", "");
+				        	  	gravarValor = gravarValor.replaceAll("#", "");
+				        	  	gravarValor = gravarValor.replaceAll("Saldodoperodo", "");
+				        	  	gravarValor = gravarValor.replaceAll("Saldo do período", "");
+				        	  	gravarValor = gravarValor.replaceAll("Saldo do perodo", "");
+					        	gravarValor = gravarValor.replaceAll("      ", "");
+					        	gravarValor = gravarValor.replace("CONTA JUDICIAL", "");
+					        	gravarValor =  semAcento(gravarValor.trim());
+				          }     
+	            
+				          System.out.println("RETT 3 = " + ret);	
+				            
+						 ret = ret.replace("Autor          :", "");
+						 ret = ret.replace("         Saldo do período             ", "");
+						 ret = ret.replace("          Saldo do período            ", "");
+						 ret = ret.replace("    Saldo do periodo    ", "");
+						 ret = ret.replace("/", "");
+						 ret = ret.replace("                ", "");
+						 ret = ret.replace("Reclamante", "");
+						 ret = ret.replace(":", "");
+						 ret = ret.replace("     ", "");
+						 ret = ret.replace("     Saldo do periodo ", "");
+						 ret = ret.replace("Autor", "");
+						 ret = ret.replace("     Saldo do periodo    ", "");
+						 ret = ret.replace("     Saldo do periodo    ", "");
+						 ret = ret.replace("Saldo do periodo", "");
+			           	ret = ret.replaceAll("[^\\p{ASCII}]", "");
+			           	ret = ret.replaceAll("//", "");
+			        		ret = ret.replaceAll("/", "");
+			        		ret = ret.replaceAll("#", "");
+			        		ret = ret.replaceAll("Saldodoperodo", "");
+			        		ret = ret.replaceAll("Saldo do período", "");
+			        		ret = ret.replaceAll("Saldo do perodo", "");
+			        		ret = ret.replaceAll("      ", "");
+			        		ret = ret.replace("CONTA JUDICIAL", "");
+			        		
+			        		System.out.println("CONTEDUDO ret "+ret);
+			                
+			        		
+						 
+					  }
+					  //RETORNA BB
+					  ret =  semAcento(ret);
+					  return ret;
+				}
+				// SE NAO É PDF CAIXA  
+				else if(banco.equals("CEF")) //Caixa Economica Federal
+				{
+					  
+					System.out.println("EXTRATO CAIXA ECONOMICA");
+					  
+					  for (int i = 0; i < linhas.length; i++) // Le array normal
+					  {
+						  
+						  
+						  
+//				          if(i == 20 && linhas[i].length() >= 8) //PEGANDO CONTA JUDICIAL
+//				          {
+//				           		ret = linhas[i];
+//				           		ret = ret.substring(17, ret.length());  //FAZ SUBSTRING PARA TIRAR O NUMERO DE PARCELA DA STRING
+//				           		
+//				          	 System.out.println("ret-2---------"+ret);
+//				           		
+//				          }
+
+						  
+				          if(i == 25) //PEGANDO O NOME
+				          {
+				           		//ret = linhas[i];
+				           		//ret = linhas[i] +"  CJ"+ ret;
+				           		
+				        	  		ret = "";
+				        	  		ret = linhas[i];
+				           		
+				           //	 System.out.println("ret-----------"+ret);
+				           		
+				           		ret = ret.replaceAll("[^\\p{ASCII}]", "");
+				           		ret = ret.replaceAll("//", "");
+				           		ret = ret.replaceAll("/", "");
+				           		ret = ret.replaceAll("#", "");
+				           		ret = ret.replaceAll("Saldodoperodo", "");
+					        		ret = ret.replaceAll("Saldodoperodo", "");
+					        		ret = ret.replaceAll("Anterio", "");
+
+					        		ret = ret.replaceAll("..-", "");
+				        	  		ret = ret.replaceAll("Anterio", "");
+				        	  		ret = ret.replaceAll("DEP.DINH", "");
+				        	  		ret = ret.replaceAll("DEB.AUTOR", "");
+				        	  		ret = ret.replace("Remunerao", "");
+				        	  		
+				        	  		ret = ret.replace("1", "");
+				        	  		ret = ret.replace("2", "");
+				        	  		ret = ret.replace("3", "");
+				        	  		ret = ret.replace("4", "");
+				        	  		ret = ret.replace("5", "");
+				        	  		ret = ret.replace("6", "");
+				        	  		ret = ret.replace("7", "");
+				        	  		ret = ret.replace("8", "");
+				        	  		ret = ret.replace("9", "");
+				        	  		ret = ret.replace("0", "");
+				        	  		ret = ret.replace(".", "");
+					        		// System.out.println("ret---------->"+ret);
+				           		
+				          }
+				          
+				         // System.out.println(linhas.length);
+				         // System.out.println(i);
+				          
+				          String[] array ;
+				          
+				          //PEGANDO O VALOR
+				          if(linhas.length-2 == i)//é o penultimo  Registro?
+				          {
+				        	  			
+				        	  		if(!ret.matches("[0-9]"))//contem apenas numero?
+				        	        {
+					        	  		String[] Valor = null;
+					        	  		Valor =  linhas[i].substring(5, linhas[i].length()-1).split(" ");// Pega a linha e Tira a Data que contem incialmente na linha faz Splito para pegar o segundo valor da Linha
+				        	  			
+					        	  		array =  linhas[i].substring(5, linhas[i].length()-1).split(" ");// Pega a linha e Tira a Data que contem incialmente na linha faz Splito para pegar o segundo valor da Linha	
+					        	  		
+
+					        	  		String retornando = "";
+					        	  		String ArrayValor = "";
+					        	  		retorno = "";
+					        	  		
+					        	  		gravarValor = "";
+					        	  		gravarValor = Valor[2];
+					        	  		gravarValor = gravarValor.replace("CRED JUROS", "");
+					        	  		gravarValor = gravarValor.replace("CRED", "");
+					        	  		gravarValor = gravarValor.replace("DB", "");
+					        	  		gravarValor = gravarValor.replace(" JURO", "");
+					        	  		gravarValor = gravarValor.replaceAll("[^\\p{ASCII}]", "");
+					        	  		gravarValor = gravarValor.replaceAll("#", "");
+					        	  		gravarValor = gravarValor.replaceAll("/", "_");
+					        	  		gravarValor = gravarValor.replaceAll("Anterio", "");
+					        	  		gravarValor = gravarValor.replaceAll("DEP.DINH", "");
+					        	  		gravarValor = gravarValor.replaceAll("DEB.AUTOR", "");
+						  			gravarValor = gravarValor.replace("Remunerao", "");
+					        	  		
+				        	  			System.out.println("gravarValor :>"+gravarValor);
+
+				        	        }
+				        	  		else {
+				        	  			ret =  ret; //se sim para nao dar erro envia apenas o nome
+				        	  		}
+				        	  		
+				          }
+
+					  }
+					  //RETORNA CAIXA
+					  
+					//  System.out.println("RETORNAR__________>: "+ret);	  
+					  
+					  ret =  semAcento(ret);
+					  return ret;
+				}	
+				  
+				  ret =  semAcento(ret);
+				  
+				  
+			  	
+			  	return ret;
+			  
+		  }
+		  
+		  public static String semAcento(String str) {
+		        String nfdNormalizedString = Normalizer.normalize(str, Normalizer.Form.NFD); 
+		        Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+		        return pattern.matcher(nfdNormalizedString).replaceAll("");
+		    }
+
+		  
+		  
+		  public static String clonandoPDF(String texto) {			  
+	          
+		        FileWriter arquivo;  
+		          
+		        try {  
+		            arquivo = new FileWriter(new File("/Volumes/HD/workspace/RoboPFD/Arquivo.txt"));  
+		            arquivo.write(texto);  
+		            arquivo.close();  
+		        } catch (IOException e) {  
+		            e.printStackTrace();  
+		        } catch (Exception e) {  
+		            e.printStackTrace();  
+		        }  
+			  
+			  return "";
+			  
+		  }
+
+}
---
+++ src/TelaRobo.java
@@ -0,0 +1,267 @@
+
+
+
+
+import java.awt.FileDialog;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JFileChooser;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class TelaRobo extends javax.swing.JFrame {
+	
+	static RoboPDF RoboPDF = new RoboPDF();
+
+	
+	static String nome = "";
+	static String path = "";
+	
+
+    public TelaRobo() {
+        initComponents();
+        setTitle("ROBO RENOMEAR ARQUIVO");
+        
+        UIManager.LookAndFeelInfo[] inf = UIManager.getInstalledLookAndFeels();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+    private void initComponents() {
+
+
+        jbtnBotaoExcel = new javax.swing.JButton();
+        jbtnBotaoPDF = new javax.swing.JButton();
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jtaArea = new javax.swing.JTextPane();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+
+
+        jbtnBotaoExcel.setText("PDF ORIGINAL");
+        jbtnBotaoExcel.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtnBotaoExcelActionPerformed(evt);
+            }
+        });
+
+        
+        jbtnBotaoPDF.setText("PDF DESINO");
+        jbtnBotaoPDF.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtnBotaoPDFActionPerformed(evt);
+            }
+        });
+
+        jtaArea.setEditable(false);
+       // jtaArea.setColumns(20);
+        jtaArea.setFont(new java.awt.Font("Verdana", 0, 10)); // NOI18N
+        //jtaArea.setRows(5);
+        jScrollPane1.setViewportView(jtaArea);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1)
+                    .addGroup(layout.createSequentialGroup()
+                        //.addComponent(jbtnBotao1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jbtnBotaoExcel)
+                        .addComponent(jbtnBotaoPDF)
+                        .addGap(18, 18, 18)))//
+
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jbtnBotaoPDF)
+                    .addComponent(jbtnBotaoExcel))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 124, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>                        
+
+                                       
+
+    private void jbtnBotaoExcelActionPerformed(java.awt.event.ActionEvent evt) {                                           
+    		buscarFileDialogExcel();
+
+    }    
+    
+    private void jbtnBotaoPDFActionPerformed(java.awt.event.ActionEvent evt) {                                           
+        buscarFileDialogPDF();
+    }                                          
+
+
+                                              
+
+    public static void main(String args[]) {
+        (new TelaRobo()).show();
+	      jtaArea.setText(""
+	    		  +"\n   SIGA O PROCEDIMENTO ABAIXO: \n"
+	    		  +"\n   1 - SELECIONE A PASTA ORIGEM \n"
+	    		  +"   2 - SELECIONE PASTA DESTINO  \n"
+	    		  +"\n   PARA FINALIZAR CLIQUE NO X \n"
+	    );
+    }
+
+    // Variables declaration - do not modify                     
+    private javax.swing.JScrollPane jScrollPane1;
+//    private javax.swing.JButton jbtnBotao1;
+    private javax.swing.JButton jbtnBotaoExcel;
+    private javax.swing.JButton jbtnBotaoPDF;
+//    private javax.swing.JComboBox jcbxLookAndFeel;
+    private static JTextPane jtaArea;
+    // End of variables declaration                   
+
+
+    
+    private void buscarFileDialogExcel() {
+        try {
+            
+                FileDialog fd = new FileDialog(this, "Buscar Texto", FileDialog.LOAD);
+                fd.setMultipleMode(false);
+                fd.show();
+
+                File arquivo = new File(fd.getDirectory() + fd.getFile());
+
+                if (!arquivo.isFile()) {
+                    return;
+                }
+                
+                nome = arquivo.getParent();// pegando nome excel
+              
+                jtaArea.setText(""
+      	    		  +"\n  MUITO BEM !!: \n"
+      	    		  +"\n  AGORA SELECIONE O PDF \n"
+      	    		  +"\n  APÓS SELECIONAR O PDF AGUARDE A MENSAGEM DE SUCESSO. \n"
+                	);
+                
+               // jtaArea.setText("" +"\n \n \n \n SELECIONE O PDF E AGUARDE A MENSAGEM FINALIZANDO");              
+
+
+                
+                
+        } catch (Exception e) {
+        }
+    }
+    
+    
+    private void buscarFileDialogPDF() {
+        try {
+            
+                FileDialog fd = new FileDialog(this, "Buscar Texto", FileDialog.LOAD);
+                fd.setMultipleMode(false);
+                fd.show();
+
+                File arquivo = new File(fd.getDirectory() + fd.getFile());
+
+                if (!arquivo.isFile()) {
+                    return;
+                }
+                
+                
+                	 
+                	jtaArea.setContentType("text/html"); 
+                	jtaArea.setText("<html></body><center><h3><font color=#a70104>RENOMEANDO PDF ...</font>.</h3><img src=\"http://passofundo.ifsul.edu.br/imagens/padrao/aguarde.gif\"><center></body></html>"); 
+                	
+
+                	path = arquivo.getParent();
+                rodarArquivos(path, nome);
+                
+
+                
+
+                
+                
+        } catch (Exception e) {
+        }
+    }
+    
+    
+//    private void rodarArquivos(String pasta, String pathExcel) {
+//        try {
+//        	
+//        		System.out.println("pathExcel --> "+pathExcel);
+//        		System.out.println("pasta --> "+pasta);
+//        		
+//             EFBB.excelBB = pathExcel;
+//             EFBB.caminho = pasta+"/";
+//             
+//             EFBB.init();
+//        } catch (Exception e) {
+//        }
+//    }
+    
+    
+    public void rodarArquivos(final String pathRenomearPDF, final String pastaPDF) {
+		new Thread() {
+			
+			@Override
+			public void run() {
+				
+	        	//	System.out.println("pastaPDF --> "+pastaPDF);
+	        //	System.out.println("pathRenomearPDF --> "+pathRenomearPDF);
+	        		
+	        		//MAC IOS e WINDOWS
+	        		RoboPDF.caminho = pastaPDF+"/";
+	        		RoboPDF.destino = pathRenomearPDF+"/";
+	        		
+//	        		//WINDOWS
+//	        		RoboPDF.caminho = pastaPDF+"\\";
+//	        		RoboPDF.destino = pathRenomearPDF+"\\";
+	        		
+	        		
+	        		while(true){
+	             
+		             try 
+		             {
+		            	 	RoboPDF.init();
+		             } catch (IOException e) 
+		             {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						try {
+							Thread.sleep( 30000 );
+						} catch (InterruptedException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+						run();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+		             
+		             try {
+						Thread.sleep( 30000 );
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						run();
+					}
+		             
+	        		            
+	        		}
+			}
+		}.start();
+
+	}
+}
